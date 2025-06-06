39b19079af69a7e63bcfcef148b483b576b9818e
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2025-06-06 16:52:26 +0700

    full base

---
+++ STM32CubeIDE/Debug/Application/User/generated/image_glass_theme_images_widgets_button_regular_height_50_small_round_active.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_glass_theme_images_widgets_button_regular_height_50_small_round_active.o: \
+ D:/VSCode/2024.2/embedded/CK/PingPongGame/TouchGFX/generated/images/src/__generated/image_glass_theme_images_widgets_button_regular_height_50_small_round_active.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ STM32CubeIDE/Debug/Drivers/BSP/Components/stmpe811.d
@@ -0,0 +1,8 @@
+Drivers/BSP/Components/stmpe811.o: \
+ D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/stmpe811.c \
+ D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/stmpe811.h \
+ D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/../Common/ts.h \
+ D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/../Common/io.h
+D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/stmpe811.h:
+D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/../Common/ts.h:
+D:/VSCode/2024.2/embedded/CK/PingPongGame/Drivers/BSP/Components/stmpe811/../Common/io.h:
