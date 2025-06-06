dc54216455c7e43dd7ac99325335543be5d684ef
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2025-06-06 19:34:13 +0700

    accept fist clean project

--- STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
+++
@@ -1,4 +0,0 @@
-Application/User/TouchGFX/App/app_touchgfx.o: \
- D:/VSCode/2024.2/embedded/CK/PingPongGame/TouchGFX/App/app_touchgfx.c \
- D:/VSCode/2024.2/embedded/CK/PingPongGame/TouchGFX/App/app_touchgfx.h
-D:/VSCode/2024.2/embedded/CK/PingPongGame/TouchGFX/App/app_touchgfx.h:
