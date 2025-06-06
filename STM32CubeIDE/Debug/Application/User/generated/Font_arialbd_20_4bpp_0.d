c0b78ad6e3454b5f4eb8dd985a0e33e168f23307
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2025-06-06 19:34:13 +0700

    accept fist clean project

--- STM32CubeIDE/Debug/Application/User/generated/Font_arialbd_20_4bpp_0.d
+++
@@ -1,6 +0,0 @@
-Application/User/generated/Font_arialbd_20_4bpp_0.o: \
- D:/VSCode/2024.2/embedded/CK/PingPongGame/TouchGFX/generated/fonts/src/Font_arialbd_20_4bpp_0.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Types.hpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Types.hpp:
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
--- STM32CubeIDE/Debug/Application/User/generated/image_icon_theme_images_action_home_filled_30_30_000000.svg.d
+++
@@ -1,4 +0,0 @@
-Application/User/generated/image_icon_theme_images_action_home_filled_30_30_000000.svg.o: \
- D:/VSCode/2024.2/embedded/CK/PingPongGame/TouchGFX/generated/images/src/__generated/image_icon_theme_images_action_home_filled_30_30_000000.svg.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
