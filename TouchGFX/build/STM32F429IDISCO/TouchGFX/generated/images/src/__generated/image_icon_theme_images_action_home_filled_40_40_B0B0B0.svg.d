e7c3a7d536691cda3346732f11772369086106dc
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2025-06-30 17:04:51 +0700

    Cập nhật để build được trên STM32IDE

--- TouchGFX/build/STM32F429IDISCO/TouchGFX/generated/images/src/__generated/image_icon_theme_images_action_home_filled_40_40_B0B0B0.svg.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/build/STM32F429IDISCO/TouchGFX/generated/images/src/__generated/image_icon_theme_images_action_home_filled_40_40_B0B0B0.svg.o: \
- TouchGFX/generated/images/src/__generated/image_icon_theme_images_action_home_filled_40_40_B0B0B0.svg.cpp \
- Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
